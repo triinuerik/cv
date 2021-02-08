0502adf9a42a45bc449c52cc633e343e534ba346
Triinu Erik <triinue96@gmail.com>
2021-02-08 20:41:23 +0100

    Make active link bolder

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import './NavBar.scss'
 
 export const NavBar: React.FC = () => {
-  return(
+  const location = useLocation();
+
+  const isActiveLink = (pathname: string) => location.pathname === pathname ? 'active' : '';
+
+  return (
     <nav className='nav-bar'>
-      <Link to="/" className='nav-item'>CV</Link>
-      <Link to="/about" className='nav-item'>About me</Link>
-      <Link to="/contact" className='nav-item'>Contact</Link>
+      <Link to="/" className={`nav-item ${isActiveLink('/')}`}>CV</Link>
+      <Link to="/about" className={`nav-item ${isActiveLink('/about')}`}>About me</Link>
+      <Link to="/contact" className={`nav-item ${isActiveLink('/contact')}`}>Contact</Link>
     </nav>    
   )
 }
