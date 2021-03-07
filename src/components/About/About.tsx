a9ba5590f6e8033511c773b0c4ae62f60446d744
Triinu Erik <triinue96@gmail.com>
2021-03-07 13:36:18 +0100

    Add About Me and Contact content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import AboutMeImage from '../__assets__/about-me-image.jpg';
+import './About.scss';
 
 export const About: React.FC = () => {
   return(
     <div className='about'>
-
+        <img src={AboutMeImage} alt='Picture of Triinu Erik' className='about-me-image'/>
+        <div>
+            I'm a frontend developer 
+        </div>
     </div>
   )
 }
