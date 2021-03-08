130e7e12a85f0de85e024c828a7255da76cb4d85
Triinu Erik <triinue96@gmail.com>
2021-03-08 20:53:44 +0100

    Update about text and fix small styling issues

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -9,10 +9,12 @@ export const About: React.FC = () => {
     <div className='about'>
       <img src={AboutMeImage} alt='Author, Triinu Erik' className='about-me-image' />
       <div className='about-me-text'>
+        <h2>About me</h2>
         <p>
           Hello, I'm Triinu.
           I'm a front-end developer with 2 years of experience in building web applications.
-          I love and enjoy programming in React, but I also feel comfortable with Java and Python.
+          I love and enjoy programming in React, a topic close to my heart is building accessible interfaces.
+          Having said that, I also feel comfortable with Java and Python.
           In my free time I enjoy running, playing ukulele and video games and teaching tricks to my 3 rats.
         </p>
         <div className='contact'>
