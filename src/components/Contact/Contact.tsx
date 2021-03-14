a3705b41b752a22d673433660edff31785195085
Triinu Erik <triinue96@gmail.com>
2021-03-14 10:06:34 +0100

    Fix contacts on mobile

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,7 +6,7 @@ import './Contact.scss';
 export const Contact: React.FC = () => {
   return (
     <div className='contact'>
-      <div className='contact-row'>
+      <div className='contact-column'>
         <div className='contact-item'>
           <div className='contact-icon'><IoIosCall /></div>
           <div>+372 59 022 850</div>
@@ -16,7 +16,7 @@ export const Contact: React.FC = () => {
           <a href='https://www.linkedin.com/in/triinu-erik/' target='_blank' rel='noreferrer'>linkedin.com/in/triinu-erik/</a>
         </div>
       </div>
-      <div className='contact-row'>
+      <div className='contact-column'>
         <div className='contact-item'>
           <div className='contact-icon'><IoIosMail /></div>
           <div>triinuerik96@gmail.com</div>
